a20d69caa3a134442b41335c274f09066cd2b4b3
DoraNotTheExplorer <95859066+jwvillareal184@users.noreply.github.com>
2025-04-02 13:12:36 +0800

    update the forgot password functionalble

--- src/RequestNewPassword.tsx
+++ src/RequestNewPassword.tsx
@@ -21,7 +21,7 @@ export default function RequestNewPassword() {
 
         // Use Supabase Auth to send a password reset email
         const { error } = await supabase.auth.resetPasswordForEmail(email, {
-            redirectTo: "http://localhost:5173/reset-password" // Change to your actual frontend URL
+            redirectTo: "/reset-password" // Change to your actual frontend URL
         });
 
         if (error) {
